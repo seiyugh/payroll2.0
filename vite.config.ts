78166228f53ad072d2285fb12cf18909d8362c16
seiyugh <llester996@gmail.com>
2025-04-02 14:39:40 +0800

    fixed deps

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 import laravel from 'laravel-vite-plugin';
 import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
-import fs from 'node:fs';
 
 export default defineConfig({
     plugins: [
@@ -26,14 +25,8 @@ export default defineConfig({
     server: {
         hmr: {
             host: process.env.APP_URL?.replace(/https?:\/\//, ''),
-            protocol: 'wss',
-        },
-        // Correct https configuration
-        https: process.env.NODE_ENV === 'production' ? {
-            // You can provide proper certs if needed, or use basic https
-            key: fs.readFileSync('path/to/key.pem'),
-            cert: fs.readFileSync('path/to/cert.pem'),
-        } : false,
+            protocol: process.env.NODE_ENV === 'production' ? 'wss' : 'ws',
+        }
     },
     build: {
         assetsInlineLimit: 0,
